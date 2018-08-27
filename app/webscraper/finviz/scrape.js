0afc114d85fe60260a7a57261b9cf6b67ef643cd
Anson <ansonervin@yahoo.com>
2018-08-27 11:37:36 -0400

    Added some more functionality to the app

--- app/webscraper/finviz/scrape.js
+++ app/webscraper/finviz/scrape.js
@@ -1,78 +1,88 @@
 var finviz = require('finviz');
+var fs = require('fs')
+let stocks = ['NYMT','GPX']
+let stockList = [];
 
+/* 
+	To get data on diffent stocks, simply change the stocks list.
+	To filter different stock data:
+		Adjust filterStockProps
+			Log the value of the passed data object and adjust the stockData object
+			Adjust createStockString 
+				Change the template string to reflect your changes
+		
+*/
+(function(){
+	getStocks()
+	flow()
+}())
 
-function getStockList(list=[]){
-	stockList = list
+function createFile(){	
+	if (stockList.length >= 2){
+		stockList.forEach(stock => {
+			let string = createStockString(stock);
+			console.log(string)
+			fs.appendFile('StockData.txt', string, (err) => {
+			if (err) throw err;
+			console.log('The file has been saved!\n', string);
+			console.log('hey there')
+			});
+		})
+	}
+}
+function createStockString(stock, ...args){
+	return `So heres data for ${stock.ticker}\n 
+	Price: ${stock.Price}\n
+	Dividend Percent: ${stock.dividendPercent}\n
+	Returns : \n
+		ROE: ${stock.ROE}\n
+		ROA: ${stock.ROA}\n
+		ROI: ${stock.ROI}\n
+	Margins: \n
+		Gross Margin: ${stock.grossMargin}\n
+		Profit Margin: ${stock.profitMargin} \n
+		operatingMargin: ${stock.operatingMargin} \n
+	EPSPast5Y: ${stock.EPSPast5Y}\n\n`
+}
+
+function filterStockProps(data={},ticker){
+	console.log(data);
+	let stockData = {
+		ticker: ticker,
+		dividendPercent : data['Dividend %'],
+		ROE: data.ROE,
+		ROA: data.ROA,
+		ROI: data.ROI,
+		EPSPast5Y: data['EPS past 5Y'],
+		Price: data.Price,
+		grossMargin: data['Gross Margin'],
+		profitMargin: data['Profit Margin'],
+		operatingMargin: data['Oper. Margin']
+	}
+	console.log(stockData);
+	stockList.push(stockData);
 	return stockList
 }
-
-function filterStockProps(data={}){
-	let stockData = {
-		ticker: data.Ticker,
-		dividendPercent : data.Dividend,
-		ROE: data.ROE,
-		// operatingMargin: data.operatingMargin
-	}
-	console.log('Returning this stock data', stockData)
-	return stockData;
-}
-
 function finvizCall(ticker){
 	if(ticker !== ''){
 	        finviz.getStockData(ticker)
 	            .then(data => {
-	            	console.log(`Symbol ${data.Ticker} \n Dividend:${data.Dividend} \n ROE: ${data.ROE}`)
-	            	return data;
+	            	filterStockProps(data,ticker);
+	            })
+	            .then(data => {
+	            	createFile()
 	            })
 	            .catch(err => console.error(err.stack ? err.stack : err));
 		} 
 }
-function stocksGetProps(list, callFinviz, callback){
-	let stockList = [];
-	tickers.forEach((stock)=>{
-		callFinviz(stock);
-	});
-	console.log('Returning stockList')
+
+function getStocks(){
+	stocks.forEach(stock => finvizCall(stock));
 }
-
-function Stock(ticker){
-	this.ticker = ticker
+function flow(){
+	if (stockList === []){
+		setTimeOut(flow, 3000);
+	} else{
+		return
+	}
 }
-Stock.prototype.ticker = '';
-
-function StockPortfolio(){}
-
-StockPortfolio.prototype.createStocks = (tickers) =>{
-	let stockList = []
-	tickers.forEach(ticker => {
-		let stock = new Stock(ticker);
-		stockList.push(stock)
-	});
-	return stockList
-}
-StockPortfolio.prototype.flow = (list)=>{
-	let stockData = [];
-	list.forEach(stock => {
-		let afterCall = new Promise((resolve,reject)=>{
-			resolve(finvizCall(stock))
-		})
-		afterCall.then(data => {
-			stockData.push(filterStockProps(data))
-		})
-	})
-}
-
-
-let bayArea = new StockPortfolio()
-let stocks = bayArea.createStocks(['NYMT','GPX'])
-bayArea.flow(stocks);
-exports.filterStockProps = filterStockProps;
-exports.getStockList = getStockList;
-exports.finvizCall = finvizCall;
-exports.stockGetProps = stocksGetProps;
-exports.Stock = Stock;
-exports.StockPortfolio = StockPortfolio;
-
-/* need to create a dataFilter object with req ratios:
-	need to create a calculate function that uses the data from getStock
-	 */
