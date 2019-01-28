2efa3768210094aeef002f62c240dcca3239e846
Anson Ervin <ansonervin@yahoo.com>
2019-01-27 19:21:40 -0600

    Changed route names and also changed test name

--- app/test/ping-spec.ts
+++ app/test/ping-spec.ts
@@ -5,7 +5,7 @@ import supertest from 'supertest';
 const request = supertest(app)
 
 describe('Ping GET route', () =>{
-    it('should return calling finviz', done => {
+    it('should return a 200 for ping GET route', done => {
         request
         .get('/api/ping')
         .expect(200)
