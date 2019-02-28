880ebaabac77cc735377e8248e07862d10b22b30
Anson Ervin <ansonervin@yahoo.com>
2019-02-28 18:19:17 -0500

    Created tests for finviz.ts and adjusted test on router-spec

---
+++ app/test/finviz-spec.ts
@@ -0,0 +1,76 @@
+import 'mocha';
+import { app } from '../app'
+import * as assert from 'assert';
+import supertest from 'supertest';
+import { StockList } from '../lib/finviz';
+
+const request = supertest(app)
+
+describe('Stocklist class', () => { 
+    const stockList = new StockList();
+    const expectedStockData = { 
+        ticker: 'NYMT',
+        dividendPercent: '13.33%',
+        ROE: '13.40%',
+        ROA: '0.80%',
+        ROI: '0.80%',
+        EPSPast5Y: '-11.60%',
+        price: '6.00',
+        grossMargin: '30.40%',
+        profitMargin: '18.70%',
+        operatingMargin: '23.20%',
+        marketCap: '1.02B' }
+    const sampleStockData = {
+        'Dividend %': '13.4%',
+        ROE: '2,3%',
+        ROA: '5.4%',
+        ROI: '9.23%',
+        'EPS past 5Y': '4.4%',
+        'Price': '4.32',
+        'Gross Margin': '4.32%',
+        'Profit Margin': '7.23%',
+        'Oper. Margin': '4.32%',
+        'Market Cap': '1.03B',
+        exchange: 'NASDAQ', 
+        dividend: '$10.40' 
+    }
+
+    const expectedFilteredStockData = { 
+        ticker: 'TEST',
+        dividendPercent: '13.4%',
+        ROE: '2,3%',
+        ROA: '5.4%',
+        ROI: '9.23%',
+        EPSPast5Y: '4.4%',
+        price: '4.32',
+        grossMargin: '4.32%',
+        profitMargin: '7.23%',
+        operatingMargin: '4.32%',
+        marketCap: '1.03B' }
+
+   it('should get stock list', () => {
+        assert.deepEqual(stockList.getStockList(), [], 'should be an empty array')
+   })
+   
+   it('should retieve stock data from finviz', function(done) {
+    this.timeout (9000);
+    stockList.getStockDataFromFinviz('NYMT').then(stock => {
+        assert.deepEqual(stock, expectedStockData);  
+        done()
+      }, done)
+    })
+
+    it('should push to stockList property', () => {
+       stockList.pushToStockList(expectedStockData)
+       assert.equal(stockList.getStockList().length, 1)
+    })
+
+    it('should remove stock list', () => {
+        stockList.shiftStockList()
+        assert.equal(stockList.getStockList().length, 0)
+    })
+
+    it('should filter stock properties correctly', () => {
+        assert.deepEqual(StockList.filterStockProps(sampleStockData, 'TEST'), expectedFilteredStockData)
+    })
+})
--- app/test/router-spec.ts
+++ app/test/router-spec.ts
@@ -46,75 +46,86 @@ describe('GET User route', () => {
             }
         });
     })
-
 })
 
 describe('GET Stock list route', () => {    
-    const expectedStockList = [ { _id: '5c54568a6855e61af094a49e',
-    ticker: 'AAPL',
-    dividendPercent: '1.75%',
-    ROE: '48.70%',
-    ROA: '16.00%',
-    ROI: '26.60%',
-    EPSPast5Y: '16.50%',
-    price: '166.44',
-    grossMargin: '38.30%',
-    profitMargin: '22.40%',
-    operatingMargin: '26.70%',
-    marketCap: '799.18B',
-    __v: 0 },
-    { _id: '5c54568a6855e61af094a4a0',
-    ticker: 'CRON',
-    dividendPercent: '-',
-    ROE: '-',
-    ROA: '-',
-    ROI: '-',
-    EPSPast5Y: '-',
-    price: '19.68',
-    grossMargin: '-',
-    profitMargin: '-',
-    operatingMargin: '-',
-    marketCap: '3.52B',
-    __v: 0 },
-    { _id: '5c54568a6855e61af094a49f',
-    ticker: 'GOOG',
-    dividendPercent: '-',
-    ROE: '-',
-    ROA: '-',
-    ROI: '-',
-    EPSPast5Y: '-',
-    price: '1116.37',
-    grossMargin: '-',
-    profitMargin: '-',
-    operatingMargin: '-',
-    marketCap: '760.15B',
-    __v: 0 },
-    { _id: '5c54568a6855e61af094a4a2',
-    ticker: 'MSFT',
-    dividendPercent: '1.76%',
-    ROE: '23.10%',
-    ROA: '7.40%',
-    ROI: '17.60%',
-    EPSPast5Y: '8.50%',
-    price: '104.43',
-    grossMargin: '65.20%',
-    profitMargin: '16.40%',
-    operatingMargin: '32.40%',
-    marketCap: '794.37B',
-    __v: 0 },
-    { _id: '5c54568a6855e61af094a4a1',
-    ticker: 'NYMT',
-    dividendPercent: '12.74%',
-    ROE: '13.90%',
-    ROA: '0.80%',
-    ROI: '0.80%',
-    EPSPast5Y: '-11.60%',
-    price: '6.28',
-    grossMargin: '30.40%',
-    profitMargin: '19.40%',
-    operatingMargin: '24.50%',
-    marketCap: '1.06B',
-    __v: 0 } ]
+    const expectedStockList = [
+        {
+          "_id": "5c54568a6855e61af094a49e",
+          "ticker": "AAPL",
+          "dividendPercent": "1.75%",
+          "ROE": "48.70%",
+          "ROA": "16.00%",
+          "ROI": "26.60%",
+          "EPSPast5Y": "16.50%",
+          "price": "166.44",
+          "grossMargin": "38.30%",
+          "profitMargin": "22.40%",
+          "operatingMargin": "26.70%",
+          "marketCap": "799.18B",
+          "__v": 0
+        },
+        {
+          "_id": "5c54568a6855e61af094a4a0",
+          "ticker": "CRON",
+          "dividendPercent": "-",
+          "ROE": "-",
+          "ROA": "-",
+          "ROI": "-",
+          "EPSPast5Y": "-",
+          "price": "19.68",
+          "grossMargin": "-",
+          "profitMargin": "-",
+          "operatingMargin": "-",
+          "marketCap": "3.52B",
+          "__v": 0
+        },
+        {
+          "_id": "5c54568a6855e61af094a49f",
+          "ticker": "GOOG",
+          "dividendPercent": "-",
+          "ROE": "-",
+          "ROA": "-",
+          "ROI": "-",
+          "EPSPast5Y": "-",
+          "price": "1116.37",
+          "grossMargin": "-",
+          "profitMargin": "-",
+          "operatingMargin": "-",
+          "marketCap": "760.15B",
+          "__v": 0
+        },
+        {
+          "_id": "5c54568a6855e61af094a4a2",
+          "ticker": "MSFT",
+          "dividendPercent": "1.76%",
+          "ROE": "23.10%",
+          "ROA": "7.40%",
+          "ROI": "17.60%",
+          "EPSPast5Y": "8.50%",
+          "price": "104.43",
+          "grossMargin": "65.20%",
+          "profitMargin": "16.40%",
+          "operatingMargin": "32.40%",
+          "marketCap": "794.37B",
+          "__v": 0
+        },
+        {
+          "_id": "5c54568a6855e61af094a4a1",
+          "ticker": "NYMT",
+          "dividendPercent": "12.74%",
+          "ROE": "13.90%",
+          "ROA": "0.80%",
+          "ROI": "0.80%",
+          "EPSPast5Y": "-11.60%",
+          "price": "6.28",
+          "grossMargin": "30.40%",
+          "profitMargin": "19.40%",
+          "operatingMargin": "24.50%",
+          "marketCap": "1.06B",
+          "__v": 0
+        }
+      ]
     
 
     it('should return a 400 error', done => {
@@ -143,9 +154,3 @@ describe('GET Stock list route', () => {
         });
     })
 })
-
-
-
-// 1. is the difference between resnet 34 and 50 the amount of validation sets that are used in training
-// 2. when they say layers is that comparable to back propogation in deep learning where there are loss functions and input output layers
-
