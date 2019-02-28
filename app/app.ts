1792b65816a1ea918d1917a8b15f723bc718be97
Anson Ervin <ansonervin@yahoo.com>
2019-02-28 16:03:21 -0500

    Refactored postStockList route

--- app/app.ts
+++ app/app.ts
@@ -14,7 +14,7 @@ app.use(express.static('public'))
 
 app.get('/api/getUser/:email/', router.getUser)
 app.get('/api/getStockList/:email/:name', router.getStockList)
-app.post('/api/postStockList/:email', router.retrieveStockData);
+app.post('/api/retrieveStockData/', router.retrieveStockData);
 app.get('/api/ping', (req, res, next) => {
   res.sendStatus(200);
 })
