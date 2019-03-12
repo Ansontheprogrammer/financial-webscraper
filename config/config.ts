82dc1caebb7d401d93c949790abedbe81e1ba6a6
Anson Ervin <ansonervin@yahoo.com>
2019-03-12 02:52:12 -0400

    Moved key to new config directory

---
+++ config/config.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoConnectionKey:  `mongodb://Anson:m4cGCRr2lKENVAT7@cluster0-shard-00-00-mqzwm.mongodb.net:27017,cluster0-shard-00-01-mqzwm.mongodb.net:27017,cluster0-shard-00-02-mqzwm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`,
+}
--- lib/database.ts
+++ lib/database.ts
@@ -1,4 +1,5 @@
 import mongoose, { mongo } from 'mongoose';
+import config from '../config/config';
 import { StockList } from './finviz';
 
 type STOCK_LIST = {
@@ -6,7 +7,8 @@ type STOCK_LIST = {
 	name: string,
 }
 
-const url = `mongodb://Anson:m4cGCRr2lKENVAT7@cluster0-shard-00-00-mqzwm.mongodb.net:27017,cluster0-shard-00-01-mqzwm.mongodb.net:27017,cluster0-shard-00-02-mqzwm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`
+const url = config.mongoConnectionKey;
+
 // connecting to mongo and creating schema
 mongoose.connect(url, {
 	useNewUrlParser: true,
